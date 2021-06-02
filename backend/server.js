c511027404b56710dec7d3e3f7be604db17d880f
Samikmalhotra <samikmalhotra66@gmail.com>
2021-06-02 18:34:05 +0530

    added productRoutes

--- backend/server.js
+++ backend/server.js
@@ -7,7 +7,7 @@ import morgan from 'morgan'
 // import { notFound, errorHandler } from './middleware/errorMiddleware.js'
 import connectDB from './config/db.js'
 
-// import productRoutes from './routes/productRoutes.js'
+import productRoutes from './routes/productRoutes.js'
 // import userRoutes from './routes/userRoutes.js'
 // import orderRoutes from './routes/orderRoutes.js'
 // import uploadRoutes from './routes/uploadRoutes.js'
@@ -24,7 +24,7 @@ const app = express()
 
 app.use(express.json())
 
-// app.use('/api/products', productRoutes)
+app.use('/api/products', productRoutes)
 // app.use('/api/users', userRoutes)
 // app.use('/api/orders', orderRoutes)
 // app.use('/api/upload', uploadRoutes)
