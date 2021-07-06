6b870d2386c2ee2cebc59ae8fb344f6bb2068b8c
Samikmalhotra <samikmalhotra66@gmail.com>
2021-07-06 23:30:34 +0530

    added product reducer

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -3,6 +3,8 @@ import { BrowserRouter as Router, Route } from 'react-router-dom'
 import { Container } from 'react-bootstrap'
 import Header from './components/Header'
 import Footer from './components/Footer'
+import HomeScreen from './screens/HomeScreen'
+import ProductScreen from './screens/ProductScreen'
 
 
 const App = () => {
@@ -11,6 +13,8 @@ const App = () => {
           <Header />
           <main className='py-3'>
             <Container>
+              <Route exact path='/' component={HomeScreen}></Route>
+              <Route exact path='/products/:id' component={ProductScreen}></Route>
 
             </Container>
             </main>
