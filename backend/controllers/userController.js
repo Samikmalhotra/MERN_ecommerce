bc890c023550ab535e308c8ebfaa82bca2214518
Samikmalhotra <samikmalhotra66@gmail.com>
2021-07-08 15:45:43 +0530

    added profile route

--- backend/controllers/userController.js
+++ backend/controllers/userController.js
@@ -30,17 +30,16 @@ const authUser = asyncHandler(async (req,res) => {
 const getUserProfile = asyncHandler(async (req,res) => {
     const user = await User.findById(req.user._id)
 
-    if(user && (await user.matchPassword(password))){
+    if(user ){
         res.json({
             _id: user._id,
             name: user.email,
             email: user.email,
             isAdmin: user.isAdmin,
-            token: generateToken(user._id)
         })
     }else{
-        res.status(401)
-        throw new Error('Invalid email address or password')
+        res.status(404)
+        throw new Error('User not found')
     }
 })
 export {authUser, getUserProfile}
