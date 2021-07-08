139207e5e7db9c262bf762cacbe858abf41969e9
Samikmalhotra <samikmalhotra66@gmail.com>
2021-07-08 17:46:46 +0530

    added register route

--- backend/controllers/userController.js
+++ backend/controllers/userController.js
@@ -24,6 +24,39 @@ const authUser = asyncHandler(async (req,res) => {
     }
 })
 
+// @desc    Register anew user
+// @route   POST /api/users
+// @acess   Public
+const registerUser = asyncHandler(async (req,res) => {
+    const {name, email, password} = req.body
+
+    const userExists = await User.findOne({email: email})
+
+    if(userExists){
+        res.status(400);
+        throw new Error('user already exists')
+    }
+
+    const user = new User({
+        name,
+        email,
+        password
+    })
+
+    if(user){
+        res.status(201).json({
+            _id: user._id,
+            name: user.name,
+            email: user.email,
+            isAdmin: user.isAdmin,
+            token: generateToken(user.id),
+        })
+    }else{
+        res.status(400)
+        throw new Error('Invalid user data')
+    }
+})
+
 // @desc    get user profile
 // @route   GET /api/users/profile
 // @acess   Private
@@ -42,4 +75,4 @@ const getUserProfile = asyncHandler(async (req,res) => {
         throw new Error('User not found')
     }
 })
-export {authUser, getUserProfile}
+export {authUser,registerUser, getUserProfile}
