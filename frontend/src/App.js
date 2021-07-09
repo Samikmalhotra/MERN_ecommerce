1760b1012f09fff43149fd078d8ad9d874a8af52
Samikmalhotra <samikmalhotra66@gmail.com>
2021-07-09 17:09:51 +0530

    added ProfileScreen

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,4 +1,4 @@
-import React from 'react'
+import React, { useEffect } from 'react'
 import { BrowserRouter as Router, Route } from 'react-router-dom'
 import { Container } from 'react-bootstrap'
 import Header from './components/Header'
@@ -8,8 +8,21 @@ import ProductScreen from './screens/ProductScreen'
 import CartScreen from './screens/CartScreen'
 import LoginScreen from './screens/LoginScreen'
 import RegisterScreen from './screens/RegisterScreen'
+import ProfileScreen from './screens/ProfileScreen'
+import { login } from './actions/userActions'
+import { useDispatch, useSelector } from 'react-redux'
+
 
 const App = () => {
+  const dispatch = useDispatch()
+  useEffect(()=>{
+    var x =(localStorage.getItem('userInfo'))
+    var y = JSON.parse(x);
+    console.log(y.email, y.password)
+    dispatch(login(y.email, y.password))
+
+  },[])
+
       return (
         <Router>
           <Header />
@@ -17,6 +30,7 @@ const App = () => {
             <Container>
               <Route path='/login' component={LoginScreen}></Route>
               <Route path='/register' component={RegisterScreen}></Route>
+              <Route path='/profile' component={ProfileScreen}></Route>
               <Route exact path='/' component={HomeScreen}></Route>
               <Route path='/product/:id' component={ProductScreen}></Route>
               <Route path='/cart/:id?' component={CartScreen}></Route>
