ee1955f209fb89df0be488f0e5ad26e70891c7f5
Samikmalhotra <samikmalhotra66@gmail.com>
2021-07-15 22:07:59 +0530

    added product creation route

--- backend/controllers/productController.js
+++ backend/controllers/productController.js
@@ -40,6 +40,26 @@ const deleteProduct = asyncHandler(async (req,res) => {
     res.json(products)
 })
 
+// @desc    Create a product
+// @route   POST /api/products
+// @acess   Private/Admin
+const createProduct = asyncHandler(async (req,res) => {
+    const product = new Product({
+        name: 'Sample name',
+        price: 0,
+        user: req.user._id,
+        image: '/images/sample.jpg',
+        brand: 'Sample brand',
+        category: 'Sample category',
+        countInStock: 0,
+        numReviews: 0,
+        description: 'Sample description'
+    })
+
+    const createdProduct = await product.save()
+    res.status(201).json(createdProduct)
+})
+
 export {
     getProducts,
     getProductById,
