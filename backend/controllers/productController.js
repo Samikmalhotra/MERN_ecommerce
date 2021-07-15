4606c0815a2728fb780f76be420bd6e6a7b67c2a
Samikmalhotra <samikmalhotra66@gmail.com>
2021-07-15 22:16:54 +0530

    added create and update product routes

--- backend/controllers/productController.js
+++ backend/controllers/productController.js
@@ -60,8 +60,36 @@ const createProduct = asyncHandler(async (req,res) => {
     res.status(201).json(createdProduct)
 })
 
+// @desc    Update a product
+// @route   PUT /api/products/:id  
+// @acess   Private/Admin
+const updateProduct = asyncHandler(async (req,res) => {
+    const {name, price, description, image, brand, category, countInStock} = req.body
+
+
+    const product = await Product.findById(req.params.id)
+
+    if(product){
+        product.name = name,
+        product.price = price,
+        product.description = description,
+        product.image = image,
+        product.brand = brand,
+        product.category = category,
+        product.countInStock = countInStock
+
+        const updatedProduct = await product.save();
+        res.json(updatedProduct)
+    }else{
+        res.status(404)
+        throw new Error('Product not found')
+    }
+})
+
 export {
     getProducts,
     getProductById,
-    deleteProduct
+    deleteProduct,
+    createProduct,
+    updateProduct
   }
