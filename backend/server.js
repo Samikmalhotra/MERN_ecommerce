be90bbce24c3d88179326957a70207a4dfeb5ce1
Samikmalhotra <samikmalhotra66@gmail.com>
2021-07-16 22:14:51 +0530

    added product review route

--- backend/server.js
+++ backend/server.js
@@ -18,9 +18,9 @@ connectDB()
 
 const app = express()
 
-// if (process.env.NODE_ENV === 'development') {
-//   app.use(morgan('dev'))
-// }
+if (process.env.NODE_ENV === 'development') {
+  app.use(morgan('dev'))
+}
 
 app.use(express.json())
 
@@ -32,9 +32,6 @@ app.use('/api/upload', uploadRoutes)
 // app.get('/api/config/paypal', (req, res) =>
 //   res.send(process.env.PAYPAL_CLIENT_ID)
 // )
-
-// const __dirname = path.resolve()
-// app.use('/uploads', express.static(path.join(__dirname, '/uploads')))
 
 // if (process.env.NODE_ENV === 'production') {
 //   app.use(express.static(path.join(__dirname, '/frontend/build')))
